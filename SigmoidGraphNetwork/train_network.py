5e88014ff4e6f7e9a831557f9165b9270decc98c
s8jjzimm <s8jjzimm@stud.uni-saarland.de>
2021-03-06 18:53:45 +0100

    d+

--- SigmoidGraphNetwork/train_network.py
+++ SigmoidGraphNetwork/train_network.py
@@ -12,18 +12,18 @@ import sys
 import pickle
 
 USE_GPU = True
-SERIES_ADDRESS = '../data/final/Voter/timeseries_ba20_100.pickle'
-ADJ_ADDRESS = '../data/final/edges_ba20.pickle'
+SERIES_ADDRESS = '../data/final/netrd/SIS/timeseries_ba10_1k_0.15.pickle'
+ADJ_ADDRESS = '../data/final/edges_ba10.pickle'
 SEED = 0
 BATCH_SIZE = 100
 HIDDEN_SIZE = 128
-NUM_DYN_EPOCHS_PER_CYCLE = 10
-NUM_NET_EPOCHS_PER_CYCLE = 20
+NUM_DYN_EPOCHS_PER_CYCLE = 15
+NUM_NET_EPOCHS_PER_CYCLE = 5
 NUM_CYCLES = 100
-FORMAT = 'old'
+FORMAT = 'timeseries'
 USE_GUMBEL = True
 TEMP_DROP_FACTOR = .95
-EXPERIMENTS = 5
+EXPERIMENTS = 1
 
 torch.manual_seed(SEED)
 np.random.seed(SEED)
@@ -34,7 +34,7 @@ exp_final_accs = list()
 exp_final_tprs = list()
 exp_final_fprs = list()
 for _ in range(EXPERIMENTS):
-    logger = lo.Logger('GGN_logs/final/Voter_ba20_100' if USE_GUMBEL else 'SGN_logs/EXP_SIS_FIXED_ba10', original_terminal=orig_terminal)
+    logger = lo.Logger('GGN_logs/trash' if USE_GUMBEL else 'SGN_logs/EXP_SIS_FIXED_ba10', original_terminal=orig_terminal)
     sys.stdout = logger
 
     print(SERIES_ADDRESS)
