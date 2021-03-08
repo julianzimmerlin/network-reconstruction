ae856513597cbc443306d929c3a6321ca4885840
julianzimmerlin <s8jjzimm@stud.uni-saarland.de>
2021-03-08 18:40:22 +0100

    zwischenstand

--- GA/main_nodewise.py
+++ GA/main_nodewise.py
@@ -8,19 +8,19 @@ import utils as ut
 import search_utils as su
 
 SEED = 0
-SERIES_ADDRESS = r'../data/final/cml/timeseries_ba10_5k_4_onetake.pickle'
-ADJ_ADDRESS = r'../data/final/edges_ba10.pickle'
+SERIES_ADDRESS = r'../data/final/cml/timeseries_ba20_5k_4_onetake.pickle'
+ADJ_ADDRESS = r'../data/final/edges_ba20.pickle'
 BATCH_SIZE = 100
 HIDDEN_SIZE = 128
 NUM_DYN_EPOCHS_INIT = 40
-NUM_DYN_EPOCHS = 40
+NUM_DYN_EPOCHS = 10
 DETECT_EARLY_CONVERGENCE = False
 RESET_DYN_LEARNER_EVERY_NTH_GEN = 5
 POP_SIZE = 10
 NEWPOP_SIZE = 40
 NUM_GEN = 45
 USE_NODEWISE_EVALUATION = False
-USE_EVALEPOCH_FOR_GUIDED_MUTATION = False
+USE_EVALEPOCH_FOR_GUIDED_MUTATION = True
 USE_DYNAMIC_MUTATIONS = True
 USE_DETERMINISTIC_EVAL = False
 FORMAT = 'standard'
@@ -53,7 +53,7 @@ exp_final_accs = list()
 exp_final_tprs = list()
 exp_final_fprs = list()
 for _ in range(EXPERIMENTS):
-    logger = lo.Logger('GA_logs/final/cml_ba10_5k_4_onetake_grad_tryout', original_terminal=orig_terminal)
+    logger = lo.Logger('GA_logs/final/cml_ba20_5k_4_onetake_eval_tryout', original_terminal=orig_terminal)
     sys.stdout = logger
 
     evaluator = ev.Evaluator(SERIES_ADDRESS, NUM_DYN_EPOCHS, DETECT_EARLY_CONVERGENCE, BATCH_SIZE, HIDDEN_SIZE, FORMAT, not USE_EVALEPOCH_FOR_GUIDED_MUTATION, USE_NODEWISE_EVALUATION, DETERMINISTIC=USE_DETERMINISTIC_EVAL)
