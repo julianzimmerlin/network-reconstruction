72189580b4502393ff00d3ab623e1c848e388339
julianzimmerlin <s8jjzimm@stud.uni-saarland.de>
2021-03-09 09:01:29 +0100

    zwischenstand

--- GA/main_nodewise.py
+++ GA/main_nodewise.py
@@ -8,12 +8,12 @@ import utils as ut
 import search_utils as su
 
 SEED = 0
-SERIES_ADDRESS = r'../data/final/cml/timeseries_ba20_1k_3.5_restart.pickle'
+SERIES_ADDRESS = r'../data/final/netrd/SIS/timeseries_ba20_10k_0.1.pickle'
 ADJ_ADDRESS = r'../data/final/edges_ba20.pickle'
 BATCH_SIZE = 100
 HIDDEN_SIZE = 128
-NUM_DYN_EPOCHS_INIT = 200
-NUM_DYN_EPOCHS = 200
+NUM_DYN_EPOCHS_INIT = 20
+NUM_DYN_EPOCHS = 20
 DETECT_EARLY_CONVERGENCE = False
 RESET_DYN_LEARNER_EVERY_NTH_GEN = 1
 POP_SIZE = 10
@@ -23,7 +23,7 @@ USE_NODEWISE_EVALUATION = False
 USE_EVALEPOCH_FOR_GUIDED_MUTATION = False
 USE_DYNAMIC_MUTATIONS = True
 USE_DETERMINISTIC_EVAL = False
-FORMAT = 'standard'
+FORMAT = 'timeseries'
 EXPERIMENTS = 5
 
 CONTINUATION = False
@@ -61,7 +61,7 @@ exp_final_accs = list()
 exp_final_tprs = list()
 exp_final_fprs = list()
 for _ in range(EXPERIMENTS):
-    logger = lo.Logger('GA_logs/final/cml_ba20_1k_3.5_restart_grad', original_terminal=orig_terminal)
+    logger = lo.Logger('GA_logs/final/SIS_ba20_10k_grad', original_terminal=orig_terminal)
     sys.stdout = logger
 
     evaluator = ev.Evaluator(SERIES_ADDRESS, NUM_DYN_EPOCHS, DETECT_EARLY_CONVERGENCE, BATCH_SIZE, HIDDEN_SIZE, FORMAT, not USE_EVALEPOCH_FOR_GUIDED_MUTATION, USE_NODEWISE_EVALUATION, DETERMINISTIC=USE_DETERMINISTIC_EVAL)
