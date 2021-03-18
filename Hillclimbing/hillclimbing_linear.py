2bffec89c5517e4a9a9acc6b372d895b0404919e
s8jjzimm <s8jjzimm@stud.uni-saarland.de>
2021-03-18 22:02:35 +0100

    li

--- Hillclimbing/hillclimbing_linear.py
+++ Hillclimbing/hillclimbing_linear.py
@@ -11,7 +11,7 @@ import copy
 import search_utils as su
 
 SEED = 0
-SERIES_ADDRESS = r'../data/final/cml/timeseries_ba20_1k_4_restart.pickle'
+SERIES_ADDRESS = r'../data/final/cml/timeseries_ba20_1k_3.5_restart.pickle'
 ADJ_ADDRESS = r'../data/final/edges_ba20.pickle'
 BATCH_SIZE = 100
 HIDDEN_SIZE = 128
@@ -19,13 +19,13 @@ NUM_DYN_EPOCHS = 200
 DETECT_EARLY_CONVERGENCE = False
 FORMAT = 'standard'
 USE_EVALEPOCH_FOR_GUIDED_MUTATION = False
-USE_NODEWISE_LOSS = False
+USE_NODEWISE_LOSS = True
 USE_DYNAMIC_STEPS = True
 NUM_GEN = 45
 DETERMINISTIC_EVAL = False
 RANDOM = False
 FREE_WALK = False
-EXPERIMENTS = 5
+EXPERIMENTS = 1
 
 CONTINUATION = False
 CONT_ADDRESS = './hillclimbing_logs/linear/final/heuristics_comp/deterministic/cml/ba10_1k_restart_3.5_random/2021-03-04T15_54_10.394816'
@@ -38,7 +38,7 @@ exp_final_accs = list()
 exp_final_tprs = list()
 exp_final_fprs = list()
 for _ in range(EXPERIMENTS):
-    logger = lo.Logger('hillclimbing_logs/linear/final/cml_ba20_1k_4_restart_grad', original_terminal=orig_terminal)
+    logger = lo.Logger('hillclimbing_logs/linear/final/nodewise/cml/ba20_1k_3.5', original_terminal=orig_terminal)
     sys.stdout = logger
     print(SERIES_ADDRESS)
     print(ADJ_ADDRESS)
