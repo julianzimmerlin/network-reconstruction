3e982bb573a035a842d1dcbcf3adb852f4ea2e27
julianzimmerlin <s8jjzimm@stud.uni-saarland.de>
2021-03-26 23:35:15 +0100

    lk

--- Hillclimbing/hillclimbing_linear.py
+++ Hillclimbing/hillclimbing_linear.py
@@ -11,8 +11,8 @@ import copy
 import search_utils as su
 
 SEED = 0
-SERIES_ADDRESS = r'../data/final/cml/timeseries_ba100_1k_4.pickle'
-ADJ_ADDRESS = r'../data/final/edges_ba100.pickle'
+SERIES_ADDRESS = r'../data/final/cml/timeseries_ba50_1k_4.pickle'
+ADJ_ADDRESS = r'../data/final/edges_ba50.pickle'
 BATCH_SIZE = 100
 HIDDEN_SIZE = 128
 NUM_DYN_EPOCHS = 200
@@ -38,7 +38,7 @@ exp_final_accs = list()
 exp_final_tprs = list()
 exp_final_fprs = list()
 for _ in range(EXPERIMENTS):
-    logger = lo.Logger('hillclimbing_logs/linear/final/nodewise/cml/ba100_1k_4', original_terminal=orig_terminal)
+    logger = lo.Logger('hillclimbing_logs/linear/final/nodewise/cml/ba50_1k_4', original_terminal=orig_terminal)
     sys.stdout = logger
     print(SERIES_ADDRESS)
     print(ADJ_ADDRESS)
